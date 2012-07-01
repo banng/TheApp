f82a43fc1c6dd77ad0f83a6d68c844b5f6831632
unknown <ihu@LONN0172.ads.zuehlke.com>
2012-07-01 14:18:50 +0100

    Fixed bug regarding definition of SPA

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,8 +3,8 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.contains("SPA2012")) {
-            return "SPA is a conference";
+        if (query.toUpperCase().contains("SPA2012")) {
+            return "SPA is a conference run by the eponymous BCS specialist group";
         }
         return "";
     }
